b963950e91ed1fb3a19257a272c1ddab4df01a81
gabvask <gabrielvascoauzier@gmail.com>
2022-07-02 16:46:33 -0400

    feat: get selected date time with DateTimePicker

--- src/screens/PlantSave/index.tsx
+++ src/screens/PlantSave/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { SvgFromUri } from "react-native-svg";
 import { PlantDTO } from "../../dtos/PlantDTO";
 
@@ -17,21 +17,65 @@ import {
   ContentText,
   RegisterButton,
   ButtonText,
+  DateTime,
 } from "./styles";
 import { BackButton } from "../../components/BackButton";
+import { ActivityIndicator, Platform } from "react-native";
+
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { format } from "date-fns";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import { PlantProps, savePlant } from "../../libs/storage";
 
 interface PlantSaveParams {
   plant: PlantDTO;
 }
 
 export function PlantSave() {
+  const theme = useTheme();
   const route = useRoute();
   const { plant } = route.params as PlantSaveParams;
 
   const { navigate } = useNavigation();
 
-  function handleRegister() {
-    navigate("Success");
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS === "ios");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleChangeTime(
+    event: DateTimePickerEvent,
+    dateTime: Date | undefined
+  ) {
+    if (Platform.OS === "android") setShowDatePicker((oldState) => !oldState);
+
+    if (dateTime) setSelectedDateTime(dateTime);
+  }
+
+  function handleOpenDateTimePickerForAndroid() {
+    setShowDatePicker((oldState) => !oldState);
+  }
+
+  async function handleRegister() {
+    const newPlant: PlantProps = {
+      ...plant,
+      hourFormatted: format(selectedDateTime, "HH:mm"),
+      dateTimeNotification: selectedDateTime,
+    };
+
+    setIsLoading(true);
+
+    try {
+      await savePlant(newPlant);
+      navigate("Success");
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
   }
 
   return (
@@ -52,9 +96,43 @@ export function PlantSave() {
         </WateringTip>
 
         <ContentText>Escolha o melhor horário para ser lembrado:</ContentText>
+        {showDatePicker && (
+          <DateTimePicker
+            value={selectedDateTime}
+            mode="time"
+            display="spinner"
+            onChange={handleChangeTime}
+          />
+        )}
+
+        {Platform.OS === "android" && (
+          <TouchableOpacity
+            onPress={handleOpenDateTimePickerForAndroid}
+            activeOpacity={0.7}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              borderColor: theme.colors.green,
+              borderWidth: 2,
+              width: "100%",
+              borderRadius: 16,
+              alignSelf: "center",
+              marginTop: 8,
+              padding: 8,
+            }}
+          >
+            <MaterialIcons name="alarm" size={32} color={theme.colors.green} />
+            <DateTime>{format(selectedDateTime.getTime(), "HH:mm")}</DateTime>
+          </TouchableOpacity>
+        )}
       </Content>
       <RegisterButton onPress={handleRegister}>
-        <ButtonText>Cadastrar planta</ButtonText>
+        {isLoading ? (
+          <ActivityIndicator color="white" />
+        ) : (
+          <ButtonText>Cadastrar planta</ButtonText>
+        )}
       </RegisterButton>
     </Container>
   );
