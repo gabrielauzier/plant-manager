c733c74f1ba31b19b5aadce0d65e2d91802aa2e8
gabvask <gabrielvascoauzier@gmail.com>
2022-07-02 16:43:48 -0400

    style: decrease font-size and add margin-bottom

--- src/components/PlantCardSecondary/styles.ts
+++ src/components/PlantCardSecondary/styles.ts
@@ -6,16 +6,18 @@ import styled from "styled-components/native";
 export const Container = styled(RectButton)`
   background-color: ${({ theme }) => theme.colors.shape};
 
+  height: 80px;
   flex-direction: row;
   align-items: center;
   padding: 16px 18px;
   border-radius: 20px;
+  margin-bottom: ${RFValue(8)}px;
 ` as unknown as typeof RectButton;
 
 export const Name = styled.Text`
   font-family: ${({ theme }) => theme.fonts.medium};
   color: ${({ theme }) => theme.colors.body_dark};
-  font-size: ${RFValue(17)}px;
+  font-size: ${RFValue(15)}px;
   margin-left: ${RFValue(16)}px;
 `;
 
